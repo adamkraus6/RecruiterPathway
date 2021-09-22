364848dae13dfd4b96f51ae6bc6253d334cea318
Mican Vollan <micanjames.vollan@mines.sdsmt.edu>
2021-09-22 12:12:27 -0600

    issue #6 - Initial create of the Recruiter Model - No functionality yet but the database does exist, I do intend to change the routes with the index page getting moved to /recruiters/list and a login page being added.

---
+++ TeamProject/Controllers/RecruitersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TeamProject.Data;
+using TeamProject.Models;
+
+namespace TeamProject.Controllers
+{
+    public class RecruitersController : Controller
+    {
+        private readonly RecruiterContext _context;
+
+        public RecruitersController(RecruiterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Recruiters
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Recruiter.ToListAsync());
+        }
+
+        // GET: Recruiters/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recruiter = await _context.Recruiter
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recruiter == null)
+            {
+                return NotFound();
+            }
+
+            return View(recruiter);
+        }
+
+        // GET: Recruiters/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recruiters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,CompanyName,Email,Password")] Recruiter recruiter)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recruiter);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recruiter);
+        }
+
+        // GET: Recruiters/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recruiter = await _context.Recruiter.FindAsync(id);
+            if (recruiter == null)
+            {
+                return NotFound();
+            }
+            return View(recruiter);
+        }
+
+        // POST: Recruiters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,CompanyName,Email,Password")] Recruiter recruiter)
+        {
+            if (id != recruiter.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recruiter);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecruiterExists(recruiter.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recruiter);
+        }
+
+        // GET: Recruiters/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recruiter = await _context.Recruiter
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recruiter == null)
+            {
+                return NotFound();
+            }
+
+            return View(recruiter);
+        }
+
+        // POST: Recruiters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var recruiter = await _context.Recruiter.FindAsync(id);
+            _context.Recruiter.Remove(recruiter);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecruiterExists(string id)
+        {
+            return _context.Recruiter.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TeamProject/Data/RecruiterContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TeamProject.Models;
+
+namespace TeamProject.Data
+{
+    public class RecruiterContext : DbContext
+    {
+        public RecruiterContext (DbContextOptions<RecruiterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TeamProject.Models.Recruiter> Recruiter { get; set; }
+    }
+}
