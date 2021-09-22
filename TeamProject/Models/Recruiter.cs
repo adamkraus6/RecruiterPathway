c5bf7bf5b51a7fa065dc75896523191f12bd969f
Mican Vollan <micanjames.vollan@mines.sdsmt.edu>
2021-09-22 11:17:05 -0600

    issue #6 - Add Recruiter Model

---
+++ TeamProject/Models/Recruiter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeamProject.Models
+{
+    public class Recruiter
+    {
+        [Required]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
