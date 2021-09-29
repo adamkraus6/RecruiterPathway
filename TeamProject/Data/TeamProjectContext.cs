c4d7974c8e2a926709f769a9cd64e23d33b2ec95
7546734 <7546734@T-SMD1032161>
2021-09-29 11:53:33 -0600

    Ref #16 Here is the start, so that seeding can begin, I will make it look better soon. In the next iteration, we will need to merge two databases because we have two of them.

---
+++ TeamProject/Data/TeamProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TeamProject.Models;
+
+namespace TeamProject.Data
+{
+    public class TeamProjectContext : DbContext
+    {
+        public TeamProjectContext (DbContextOptions<TeamProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TeamProject.Models.Student> Student { get; set; }
+    }
+}
---
+++ TeamProject/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeamProject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
