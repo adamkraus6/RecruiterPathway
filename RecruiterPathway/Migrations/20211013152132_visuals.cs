3e476bbfebcce96cb5bdfe9afcf8bfec4249c6db
Adam Kraus <adam.kraus@mines.sdsmt.edu>
2021-10-18 14:35:07 -0600

    renamed project files to product name ref #46

---
+++ RecruiterPathway/Migrations/20211013152132_visuals.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecruiterPathway.Migrations
+{
+    public partial class visuals : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ RecruiterPathway/Models/Recruiter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace RecruiterPathway.Models
+{
+    public class Recruiter : IdentityUser
+    {
+        public string Name { get; set; }
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+        [Phone]
+        [Display(Name = "Phone Number")]
+        override public string PhoneNumber { get; set; }
+        [MinLength(8)]
+        [MaxLength(64)]
+        public string Password { get; set; }
+        [EmailAddress]
+        override public string Email { get; set; }
+        //Enables validation of the username as an email since we are requiring usernames to be emails.
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        override public string UserName { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
