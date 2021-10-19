c4419a3cb367e2af53944ddf27f3b7e5f46eee9f
Mican Vollan <MicanJames.Vollan@mines.sdsmt.edu>
2021-10-19 16:23:33 -0600

    linked issue #43 - Update seeding - Added many, many more recruiters to the seeding list and force the admin user to exist.

--- RecruiterPathway/Models/SeedDatabase.cs
+++ RecruiterPathway/Models/SeedDatabase.cs
@@ -6,56 +6,117 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using RecruiterPathway.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace RecruiterPathway.Models
 {
     public class SeedDatabase
     {
-        public static void Initialize(IServiceProvider serviceProvider)
+        public static void Initialize(IServiceProvider serviceProvider, UserManager<Recruiter> userManager)
         {
-
-            using (var context = new DatabaseContext(
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "";
+;            using (var context = new DatabaseContext(
                 serviceProvider.GetRequiredService<
                     DbContextOptions<DatabaseContext>>()))
             {
                 // Look for any movies.
-                if (context.Student.Any())
+                if (!context.Student.Any())
                 {
-                    return;   // DB has been seeded
+                    // DB has been seeded
+
+
+                    resourceName = "RecruiterPathway.Models.SeedData.Students.csv";
+                    string line;
+                    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        reader.ReadLine();
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // Writes to the Output Window.
+                            Debug.WriteLine(line);
+
+                            // Logic to parse the line, separate by comma(s), and assign fields
+                            // to the Student model.
+                            string[] values = line.Split(',');
+
+                            context.Student.AddRange(
+                                new Student
+                                {
+                                    firstName = values[0],
+                                    lastName = values[1],
+                                    degree = values[2],
+                                    gradDate = DateTime.Parse(values[3])
+                                }
+                            );
+
+                            context.SaveChanges();
+                        }
+                    }
                 }
-
-                var assembly = Assembly.GetExecutingAssembly();
-                string resourceName = "RecruiterPathway.Models.SeedData.Students.csv";
-                string line;
-
+                //Seed the administrative user into the database
+                var adminuser = new Recruiter
+                {
+                    UserName = "administrator@recruiterpathway.com",
+                    Email = "administrator@recruiterpathway.com",
+                    Name = "Administrator",
+                    CompanyName = "Recruiter Pathway",
+                    PhoneNumber = "6055555555",
+                    Password = "P@$$w0rd",
+                    Id = Guid.NewGuid().ToString(),
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+                userManager.DeleteAsync(adminuser);
+                try
+                {
+                    userManager.CreateAsync(adminuser, "P@$$w0rd");
+                }
+                catch (DbUpdateException ex)
+                {
+                    //We expect this to fail, try to add the user anyways.
+                }
+                resourceName = "RecruiterPathway.Models.SeedData.Recruiters.csv";
                 using (Stream stream = assembly.GetManifestResourceStream(resourceName))
                 using (StreamReader reader = new StreamReader(stream))
                 {
                     reader.ReadLine();
+                    string line;
                     while ((line = reader.ReadLine()) != null)
                     {
-                        // Writes to the Output Window.
                         Debug.WriteLine(line);
-
-                        // Logic to parse the line, separate by comma(s), and assign fields
-                        // to the Student model.
                         string[] values = line.Split(',');
 
-                        context.Student.AddRange(
-                            new Student
+                        var data_recruiter =
+                            new Recruiter
                             {
-                                firstName = values[0],
-                                lastName = values[1],
-                                degree = values[2],
-                                gradDate = DateTime.Parse(values[3])
+                                Id = values[0],
+                                Name = values[1],
+                                CompanyName = values[2],
+                                PhoneNumber = values[3],
+                                Email = values[1].Replace(' ','.') + "@" + values[2].ToLower() + ".com",
+                                UserName = values[1].Replace(' ', '.') + "@" + values[2].ToLower() + ".com",
+                                Password = values[4] + "!8"
+                            };
+                        var test = context.Recruiter.Where(r => r.Email == values[1] + "@" + values[2].ToLower() + ".com");
+                        if (test != null) {
+                            try
+                            {
+                                userManager.CreateAsync(data_recruiter, values[4] + "!8");
                             }
-                        );
-
-                        context.SaveChanges();
+                            catch (DbUpdateException ex)
+                            {
+                                //Tell it we expect this to come up every so often, likely due to processing speed and to ignore it
+                            }
+                        }
                     }
-
                 }
+
             }
+
+
+
+
         }
     }
 }
