01f782af49b1a00f4d0ea2422c40caee564fd051
Adam Kraus <adam.kraus@mines.sdsmt.edu>
2021-10-20 15:34:58 -0600

    creation of recruiter view model

---
+++ RecruiterPathway/Models/RecruiterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecruiterPathway.Models
+{
+    public class RecruiterViewModel
+    {
+        public List<Recruiter> Recruiters { get; set; }
+    }
+}
